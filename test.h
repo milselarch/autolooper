670e49aa11724c09cb85a337683e57b4f1c761ab
jaron <jaronho@protonmail.com>
2024-03-24 22:56:16 +0800

    chore: Removed unused OUTPUT const

--- test.h
+++ test.h
@@ -1,5 +1,3 @@
-const char* OUTPUT = "output.flac";
-
 typedef struct sound_buffer {
     short* data;
     sf_count_t size;
@@ -12,3 +10,6 @@ sf_count_t find_loop_end (sndbuf* start_buf, sndbuf* end_buf, int channels);
 void copy_samples (sndbuf* src_buf, short* dst);
 
 void extend_audio (sndbuf* extended_buf, sndbuf* intro_buf, sndbuf* loop_buf, sndbuf* ending_buf, unsigned int num_loops);
+
+int loop (const char* input_filepath, unsigned int start_time, unsigned int end_time,
+          unsigned int min_length, const char* output_filepath);
